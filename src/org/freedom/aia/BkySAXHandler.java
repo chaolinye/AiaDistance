e2601e91a8398d4b4c4ad4b1bfdd2984b5d779b5
chaolinye <2285915269@qq.com>
2017-06-20 15:23:57 +0800

    sax module

---
+++ src/org/freedom/aia/BkySAXHandler.java
@@ -0,0 +1,44 @@
+package org.freedom.aia;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by chaolin on 2017/4/20.
+ * bky的sax解析器
+ */
+public class BkySAXHandler extends DefaultHandler {
+
+    private AiaProject resultMaster;
+
+    public BkySAXHandler(AiaProject resultMaster) {
+        this.resultMaster = resultMaster;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        switch (qName){
+            //只关注mutation标签的component_type属性和event_name属性
+            case "mutation":
+                String componentType = attributes.getValue("component_type");
+                String eventName = attributes.getValue("event_name");
+                if (componentType != null){
+                    resultMaster.addMutation(componentType + " "+ eventName);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+
+    }
+}
